298abed67d28e5068289d03b452cc3fe887252cc
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-04-09 17:13:01 -0400

    MVP

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const server = express();
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -9,7 +10,7 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`${req.method} Request to ${req.originalUrl} `);
+  console.log(`${req.method} Request to ${req.originalUrl} ${date.now()} `);
   next()
 };
 
